2214ba5eeb2e8677a0b63e1748fa22c27d154805
dydko <miroslaw.dyduch@gmail.com>
2024-05-19 08:10:39 +0200

    lesson 108

--- src/test/java/udemyapitestingrestassured/TC_001.java
+++ src/test/java/udemyapitestingrestassured/TC_001.java
@@ -1,9 +1,20 @@
 package udemyapitestingrestassured;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 public class TC_001 {
+
+    String studentEndPoint = "https://thetestingworldapi.com/api/studentsdetails/10278537";
+
     @Test
     public void validateStudentGetData() {
+        Response response = RestAssured.get(studentEndPoint);
+        //response.body().prettyPrint();
+        //System.out.println(response.getHeaders());
+        System.out.println(response.asString());
+        System.out.println(response.asPrettyString());
+        System.out.println(response.cookies());
     }
 }
