004b96ce73392a1d8cae7233e32b33f5a843db36
dydko <miroslaw.dyduch@gmail.com>
2024-06-23 08:17:10 +0200

    rest assured

--- src/test/java/lombok/Person456.java
+++ src/test/java/lombok/Person456.java
@@ -4,11 +4,10 @@ package lombok;
 @AllArgsConstructor
 public class Person456 {
     static final String firstname = "asd";
-    public Person456.Test123 Test123;
 
     @Data
     @AllArgsConstructor
-    public class Test123 {
+    public static class Test123 {
         private static final String ssn2 = "fdsfdsf";
     }
 }
