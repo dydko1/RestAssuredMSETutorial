78ad70a147bfac9a053eee6f0e84d09bd3d6126b
dydko <miroslaw.dyduch@gmail.com>
2024-07-07 07:19:00 +0200

    list of list

--- src/test/java/geeksforgeeks/MainListOfList2.java
+++ src/test/java/geeksforgeeks/MainListOfList2.java
@@ -8,23 +8,23 @@ import static geeksforgeeks.ListOfLists2.iterateUsingIterators;
 public class MainListOfList2 {
     public static void main(String[] args) {
         // List of Lists
-        ArrayList<List<String>> listOfLists = new ArrayList<List<String>>();
+        ArrayList<List<String>> listOfLists = new ArrayList();
 
         // Create N lists one by one
         // and append to the list of lists
         List<String> list1
-                = new ArrayList<String>();
+                = new ArrayList();
         list1.add("A");
         list1.add("B");
         listOfLists.add(list1);
 
         List<String> list2
-                = new ArrayList<String>();
+                = new ArrayList();
         list2.add("CVCCCC");
         listOfLists.add(list2);
 
         List<String> list3
-                = new ArrayList<String>();
+                = new ArrayList();
         list3.add("XXXX");
         list3.add("YYYY");
         list3.add("ZZZZ");
