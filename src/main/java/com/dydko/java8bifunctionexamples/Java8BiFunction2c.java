5c778b497797c6e0923cff7d33c59dd2881e7909
Mirosław Dyduch <m.dyduch@cez.gov.pl>
2024-09-17 08:06:02 +0200

    bifunction

--- src/main/java/com/dydko/java8bifunctionexamples/Java8BiFunction2c.java
+++ src/main/java/com/dydko/java8bifunctionexamples/Java8BiFunction2c.java
@@ -6,22 +6,22 @@ import java.util.function.Function;
 public class Java8BiFunction2c {
 
     public static void main(String[] args) {
-//https://mkyong.com/java8/java-8-bifunction-examples/
-        String res1 = convert(10, 20,
-                (x1, x2) -> Math.pow(x1, x2),
-                (s) -> "Wynik: " + s);
 
+        String res1 = converter(2, 4, (p1, p2) -> Math.pow(p1, p2), r -> "Res: " + r);
         System.out.println(res1);
 
+        String res2 = converter(10, 20, (p1, p2) -> p1 * p2, s -> "Res: " + s);
+        System.out.println(res2);
+
+        String res3 = converter("axx", "bbb", (p1, p2) -> p1 + "\t" + p2, s -> "Wynik: " + s);
+        System.out.println(res3);
+
+        String res4=converter("100","200",(a1,a2)->a1+a2, r->"Wynik2: "+r);
     }
 
-    public static <A1, A2, R1, R2> R2 convert(A1 a1, A2 a2,
-                                              BiFunction<A1, A2, R1> func,
-                                              Function<R1, R2> func2) {
-
-        return func
-                .andThen(func2)
-                .apply(a1, a2);
-
+    public static <A1, A2, R1, R2> R2 converter(A1 a1, A2 a2,
+                                                BiFunction<A1, A2, R1> func1,
+                                                Function<R1, R2> func2) {
+        return func1.andThen(func2).apply(a1, a2);
     }
 }
