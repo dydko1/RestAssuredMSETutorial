f6fe82b9933988ea72f808dae237316bcb664d0f
Miroslaw Dyduch <miroslaw.dyduch@gmail.com>
2025-10-05 07:32:32 +0200

    test

---
+++ src/test/java/trello/Ma.java
@@ -0,0 +1,14 @@
+package trello;
+
+import io.restassured.RestAssured;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Ma {
+    private static final Log log = LogFactory.getLog(Ma.class);
+
+    public static void main(String[] args) {
+
+
+    }
+}
